7bf63e9c77d4a0fc2ead663776ef5fd17ec4acde
FroseWong <tibafrose@gmail.com>
2023-01-04 18:57:42 +0800

    a link and product card fix

--- src/js/index.js
+++ src/js/index.js
@@ -5,16 +5,21 @@ const productAll = document.querySelectorAll(".product-card");
 const popularCountryAll = document.querySelectorAll(".popular-country");
 let status = true;
 
+const mouseenterFu = function () {
+  popularCountryAll.forEach((a) => a.classList.remove("active"));
+  this.classList.add("active");
+};
+
 const popularRWD = function () {
   if (window.innerWidth > 768) {
     popularCountryAll.forEach((p) => {
-      p.addEventListener("mouseenter", function (e) {
-        popularCountryAll.forEach((a) => a.classList.remove("active"));
-        p.classList.add("active");
-      });
+      p.addEventListener("mouseenter", mouseenterFu.bind(p));
     });
   } else {
     popularCountryAll.forEach((p) => p.classList.remove("active"));
+    popularCountryAll.forEach((p) =>
+      p.removeEventListener("mouseenter", mouseenterFu, true)
+    );
   }
 };
 
@@ -31,6 +36,16 @@ productAll.forEach((product) =>
   })
 );
 
-window.addEventListener("resize", function () {});
+window.addEventListener("resize", function () {
+  let nowStatus = status;
+  if (window.innerWidth > 768) {
+    status = true;
+  } else status = false;
+
+  if (nowStatus !== status) {
+    console.log("change");
+    popularRWD();
+  }
+});
 
 popularRWD();
