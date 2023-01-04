9a57406d4485b3de56defcc33a0c3b6d9b755356
TsaiHans <rc40214@gmail.com>
2023-01-04 16:47:15 +0800

    temp

--- src/js/productdetail.js
+++ src/js/productdetail.js
@@ -178,7 +178,8 @@ createApp({
       ],
       modalPeople: 1,
       modalTotal: 777,
-      modalDate:"",
+      modalDate: "",
+      field1show: true,
     };
   },
   methods: {
@@ -451,14 +452,48 @@ createApp({
       this.modalPeople++;
     },
     // ---------------結帳寫入cookie---------------
-    modalCheckout(){
-      let time = document.getElementById('datetimepicker').value;
-      document.cookie =`日期=${time}`;
-      document.cookie =`總金額=${this.modalPricetotal}`
-      document.cookie =`點數=${this.modalPoints}`;
-        let x = document.cookie;
-        console.log(x);
-    }
+    modalCheckout() {
+      let time = document.getElementById("datetimepicker").value;
+      if (time == "") {
+        let OldToday = new Date();
+        newDate = OldToday.toISOString().split("T")[0];
+      } 
+      else 
+      {
+        let oldDate = new String(time);
+        let newDate =
+          oldDate[6] +
+          oldDate[7] +
+          oldDate[8] +
+          oldDate[9] +
+          "-" +
+          oldDate[3] +
+          oldDate[4] +
+          "-" +
+          oldDate[0] +
+          oldDate[1];
+      }
+
+      // document.cookie = `日期=${newDate}`;
+      // document.cookie = `總金額=${this.modalPricetotal}`;
+      // document.cookie = `點數=${this.modalPoints}`;
+
+      sessionStorage.setItem("日期", newDate);
+      sessionStorage.setItem("總金額", this.modalPricetotal);
+      sessionStorage.setItem("點數", this.modalPoints);
+      let data = sessionStorage.getItem("日期");
+      // let  data2 = sessionStorage.getItem('總金額');
+      // let  data3 = sessionStorage.getItem('點數');
+      console.log(data);
+
+      // window.location.href = "./payment.html";
+    },
+    cle_check() {
+      this.field1show = false;
+    },
+    field1showbtn(e) {
+      this.field1show = true;
+    },
   },
   computed: {
     // ---------------總金額---------------
@@ -466,9 +501,9 @@ createApp({
       return this.modalTotal * this.modalPeople;
     },
     // ---------------點數計算滿500元1點---------------
-    modalPoints(){
-     let points=this.modalTotal * this.modalPeople;
-      return Math.floor(points / 500);
+    modalPoints() {
+      let points = this.modalTotal * this.modalPeople;
+      return Math.floor(points / 100);
     },
   },
   mounted() {
