f61e19178c96f994a6b2f34aa0cee9cbab9263e6
kk0909091868 <kk0909091868@gmail.com>
2023-01-10 11:27:24 +0800

    前端後端

---
+++ backend/member/Product.php
@@ -0,0 +1,39 @@
+<?php
+include("Connect.php");
+$path = $_POST["path"];
+// echo $path;
+
+
+$sql = 
+"SELECT p.ProductName, p.ProductPrice, p.ProductText, p.Location, p.ProductSecondType, i.ProductImgPath 
+FROM Product p
+join ProductImg i 
+on p.ProductID =  i.ProductID
+where p.ProductStatus = 1 and p.ProductType = ? ";
+$statement =  $pdo->prepare($sql);
+
+if($path == "/weeee!/dist/productlist_travel.html"){
+    $statement->bindValue(1, "sightseeing");
+}
+else
+if($path == "/weeee!/dist/productlist_travelticket.html"){
+    $statement->bindValue(1, "transticket");
+}
+else
+if($path == "/weeee!/dist/productlist_experience.html"){
+    $statement->bindValue(1, "experience");
+ }
+elseif($path == "/weeee!/dist/productlist_sightseeingticket.html"){
+    $statement->bindValue(1, "viewpointticket");
+}
+
+
+$statement->execute();
+$data = $statement->fetchAll();
+
+//回傳json
+
+echo json_encode($data);
+
+
+?>
