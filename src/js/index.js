acff7141dc99217efb0eb0f5c7b057b6dcea1c23
FroseWong <tibafrose@gmail.com>
2023-01-10 10:24:55 +0800

    fix lg breakpoint

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,8 @@
 const heartAll = document.querySelectorAll(".fa-heart");
 const productAll = document.querySelectorAll(".product-card");
 const popularCountryAll = document.querySelectorAll(".popular-country");
+const changeHeartAll = document.querySelectorAll(".change-heart");
 let status = true;
-
 const mouseenterFu = function () {
   popularCountryAll.forEach((a) => a.classList.remove("active"));
   this.classList.add("active");
@@ -23,16 +23,18 @@ const popularRWD = function () {
   }
 };
 
-heartAll.forEach((heart) =>
-  heart.addEventListener("click", function (e) {
-    e.stopPropagation();
-    console.log("clicked heart");
-  })
-);
+// heartAll.forEach((heart) =>
+//   heart.addEventListener("click", function (e) {
+//     e.stopPropagation();
+//     console.log("clicked heart");
+//   })
+// );
 
 productAll.forEach((product) =>
-  product.addEventListener("click", function () {
+  product.addEventListener("click", function (e) {
+    e.stopPropagation();
     console.log("clicked product");
+    location.href = "./productdetail.html";
   })
 );
 
@@ -47,5 +49,15 @@ window.addEventListener("resize", function () {
     popularRWD();
   }
 });
+
+changeHeartAll.forEach((changeHeart) =>
+  changeHeart.addEventListener("click", function (e) {
+    e.stopPropagation();
+    this.querySelectorAll("i").forEach((i) => {
+      if (i.classList.contains("hidden")) i.classList.remove("hidden");
+      else if (!i.classList.contains("hidden")) i.classList.add("hidden");
+    });
+  })
+);
 
 popularRWD();
