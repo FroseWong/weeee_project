e0b04e0eb4a0a23ddd0eed3dd2a8aed579247388
TsaiHans <rc40214@gmail.com>
2023-01-11 17:52:59 +0800

    COM

---
+++ src/php/Product_08.php
@@ -0,0 +1,43 @@
+<?php
+include("Connection_08.php");
+$path = $_POST["path"];
+// echo $path;
+
+
+$sql = 
+"SELECT p.ProductName, p.ProductPrice, p.ProductText, p.Location, p.ProductSecondType, i.ProductImgPath 
+FROM Product p
+join ProductImg i 
+on p.ProductID =  i.ProductID
+where p.ProductStatus = 1 and p.ProductType = ? ";
+
+
+
+
+$statement =  getcon()->prepare($sql);
+
+if($path == "/Weeee/dist/productlist_new_travel.html"){
+    $statement->bindValue(1, "sightseeing");
+}
+else
+if($path == "/Weeee/dist/productlist_new_ticket.html"){
+    $statement->bindValue(1, "transticket");
+}
+else
+if($path == "/Weeee/dist/productlist_new_exp.html"){
+    $statement->bindValue(1, "experience");
+ }
+elseif($path == "/Weeee/dist/productlist_new_ss_ticket.html"){
+    $statement->bindValue(1, "viewpointticket");
+}
+
+
+$statement->execute();
+$data = $statement->fetchAll();
+
+//回傳json
+
+echo json_encode($data);
+
+
+?>
