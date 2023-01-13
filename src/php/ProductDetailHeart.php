86aba1caaaeb39c6b4f956404be9a8d59313fb9b
Hans <rc40214@gmail.com>
2023-01-14 00:17:32 +0800

    do not del my about onclick plz

--- src/php/ProductDetailHeart.php
+++ src/php/ProductDetailHeart.php
@@ -1,16 +1,13 @@
 <?php
-require_once('./Connection_08.php');
+require_once('connection.php');
 // -----------判斷有無加過收藏---------------
 
-// $MID=1;
-// $PID=1;
 $MID=$_POST['MID'];
 $PID=$_POST['PID'];
 
 $sqlif="SELECT count(ProductID) From Favor  where MemberID='{$MID}' and ProductID='{$PID}'";
-// $sql ="SELECT FavorStatus From Favor where MemberID='{$MID}' and ProductID='{$PID}'";
 
-$statementif = getcon()->prepare($sqlif);
+$statementif = $pdo->prepare($sqlif);
 
 $statementif->execute();
 
@@ -19,16 +16,17 @@ $dataif = $statementif->fetchAll();
 foreach($dataif as $key =>$value){
     $tempif= $value[0];
 }
+
 echo ($tempif);
 // -----------如果使用者沒加過收藏---------------
 if($tempif==0){
     $sqlzero= "INSERT INTO Favor (MemberID,ProductID,FavorStatus)VALUES ('{$MID}','{$PID}',1)";
-   $statement = getcon()->prepare($sqlzero);
+   $statement = $pdo->prepare($sqlzero);
    $statement->execute();
 }
 if($tempif==1){
-    $sqlone= "UPDATE Favor SET FavorStatus=0 WHERE ProductID='{$MID}'";
-   $statement = getcon()->prepare($sqlone);
+    $sqlone= "UPDATE Favor SET FavorStatus=0 WHERE ProductID='{$PID}' AND MemberID='{$MID}'";
+   $statement = $pdo->prepare($sqlone);
    $statement->execute();
 }
 
--- src/php/Product_08.php
+++ src/php/Product_08.php
@@ -1,11 +1,11 @@
 <?php
-include("Connection_08.php");
+require_once('connection.php');
 $path = $_POST["path"];
 // echo $path;
 
 
 $sql = 
-"SELECT p.ProductName, p.ProductPrice, p.ProductText, p.Location, p.ProductSecondType, i.ProductImgPath,i.ProductID
+"SELECT *
 FROM Product p
 join ProductImg i 
 on p.ProductID =  i.ProductID
@@ -14,7 +14,7 @@ where p.ProductStatus = 1 and p.ProductType = ? ";
 
 
 
-$statement =  getcon()->prepare($sql);
+$statement =  $pdo->prepare($sql);
 
 if($path == "/Weeee/dist/productlist_new_travel.html"){
     $statement->bindValue(1, "sightseeing");
