1f27d7a758fc67b120a0dedb437706b1c562de20
emily830101 <s603860725@gmail.com>
2023-01-17 01:25:15 +0800

    後台會員管理php

---
+++ backend/php/memberStatusToggle.php
@@ -0,0 +1,19 @@
+<?php
+//DB連線資訊
+include("connection.php");
+
+$memberNumber = $_GET['memberNumber'];
+
+//建立SQL
+$sql = "Update Member
+SET MemStatus = (CASE MemStatus WHEN 1 THEN 0 WHEN 0 THEN 1 END)
+WHERE MemberNumber = ?";
+
+//執行
+$statement = $pdo->prepare($sql);
+$statement -> bindValue(1, $memberNumber);
+$statement -> execute();
+
+//頁面導回HTML
+header("Location: ../member/list.html?update=1");
+?>
