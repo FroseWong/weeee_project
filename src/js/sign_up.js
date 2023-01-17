095563ffefe6e4791631d99af230330cbaf2cd0d
ChuYuanJyun <chusmile1021@gmail.com>
2023-01-17 17:05:47 +0800

    fix password icon show and close

---
+++ src/js/sign_up.js
@@ -0,0 +1,26 @@
+var checkEye = document.getElementById("checkEye");
+var openPasswords =  document.getElementById("openPasswords");
+checkEye.addEventListener("click", function(e){
+  if(e.target.classList.contains('fa-eye')){
+    e.target.classList.remove('fa-eye');
+    e.target.classList.add('fa-eye-slash');
+    openPasswords.setAttribute('type','text')
+  }else{
+    openPasswords.setAttribute('type','password');
+    e.target.classList.remove('fa-eye-slash');
+    e.target.classList.add('fa-eye')
+  }
+});
+var checkEyeag = document.getElementById("checkEyeag");
+var openPasswordag =  document.getElementById("openPasswordag");
+checkEyeag.addEventListener("click", function(e){
+  if(e.target.classList.contains('fa-eye')){
+    e.target.classList.remove('fa-eye');
+    e.target.classList.add('fa-eye-slash');
+    openPasswordag.setAttribute('type','text')
+  }else{
+    openPasswordag.setAttribute('type','password');
+    e.target.classList.remove('fa-eye-slash');
+    e.target.classList.add('fa-eye')
+  }
+});
