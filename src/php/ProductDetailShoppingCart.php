9653400b52b96c9ae8b558ed9b08fd1c7e2b8980
TsaiHans <rc40214@gmail.com>
2023-01-17 18:14:01 +0800

    shoppingcart

--- src/php/ProductDetailShoppingCart.php
+++ src/php/ProductDetailShoppingCart.php
@@ -1,12 +1,15 @@
 <?php
 require_once('connection.php');
-// -----------判斷有無加過收藏---------------
+// -----------判斷有無加過購物車---------------
 
 $MID = $_POST['MID'];
 $PID = $_POST['PID'];
+$TAL = $_POST['TAL'];
+$NEWQTY = $_POST['QTY'];
 
-$sqlif = "SELECT count(ProductID) From Favor  where MemberID='{$MID}' and ProductID='{$PID}'";
-// SELECT count(ProductID) From Cart  where MemberID=1 and ProductID=400
+// $sqlif = "SELECT count(ProductID) From Favor  where MemberID='{$MID}' and ProductID='{$PID}'";
+$sqlif = "SELECT count(ProductID) From Cart  where MemberID='{$MID}' and ProductID='{$PID}'";
+
 $statementif = $pdo->prepare($sqlif);
 
 $statementif->execute();
@@ -17,32 +20,28 @@ foreach ($dataif as $key => $value) {
     $tempif = $value[0];
 }
 // echo ($tempif);
-// -----------如果使用者沒加過收藏---------------
+// -----------如果使用者沒加過購物車---------------
 if ($tempif == 0) {
-    $sqlzero = "INSERT INTO Favor (MemberID,ProductID,FavorStatus)VALUES ('{$MID}','{$PID}',1)";
+    // $sqlzero = "INSERT INTO Favor (MemberID,ProductID,FavorStatus)VALUES ('{$MID}','{$PID}',1)";
+    $sqlzero = "INSERT INTO Cart (ProductID,MemberID,SubTotal,Quantity)VALUES ('{$MID}','{$PID}','{$TAL}','{$QTY}')";
     // INSERT INTO Cart (ProductID,MemberID,SubTotal,Quantity)VALUES (41,1,10000,10);
     $statement = $pdo->prepare($sqlzero);
     $statement->execute();
 }
-// -----------如果使用者有加過收藏---------------
+// -----------如果使用者有加過購物車,先找已加入個數---------------
 if ($tempif == 1) {
-    $sqlsel = "SELECT * FROM Favor where ProductID='{$PID}' AND MemberID='{$MID}'";
-    // SELECT Quantity FROM Cart where ProductID=41 AND MemberID=1
+    $sqlsel = "SELECT Quantity FROM Cart where ProductID='{$PID}' AND MemberID='{$MID}'";
     $statement = $pdo->prepare($sqlsel);
     $statement->execute();
     $datasel = $statement->fetchAll();
-
     foreach ($datasel as $key => $value) {
-        $tempsel = $value[3];
+
+        $OLDQTY = $value[$key];
     }
-    // --------如果使用者已收藏---------
-    if ($tempsel == 1) {
-        $sqlone = "UPDATE Favor SET FavorStatus=0 WHERE ProductID='{$PID}' AND MemberID='{$MID}'";
-        // UPDATE Cart SET Quantity=1000 WHERE ProductID=41 AND MemberID=1;
-        $statement = $pdo->prepare($sqlone);
-        $statement->execute();
-    }
-    // --------如果使用者未收藏---------
-    echo json_encode(!$tempsel);
+    $ALLQTY = $OLDQTY + $NEWQTY;
+    // --------加總後加入購物車---------
+    $sqlone = "UPDATE Cart SET Quantity=$ALLQTY WHERE ProductID='{$PID}' AND MemberID='{$MID}'";
+    $statement = $pdo->prepare($sqlone);
+    $statement->execute();
+    // echo json_encode(!$tempsel);
 }
-?>
