e20fb9d4ebd97b8a1a52ca62a9e8baaa509488fb
Ning Kuo <g810306@gmail.com>
2023-01-18 11:34:11 +0800

    add shoppingcart php

---
+++ src/php/shoppingcart.php
@@ -0,0 +1,28 @@
+<?php
+$memberID = "6"; //TODO 先寫死，到時候登入功能做好，可從 php session取得會員編號
+
+//DB連線資訊
+include("connection.php");
+
+//建立SQL語法
+$sql = "select p.productName, p.productPrice, i.productImgpath2, c.quantity, c.date, c.select
+from product p
+join productimg i
+on p.productID = i.productID
+join cart c
+on p.productID = c.productID
+where memberID = 6;
+";
+
+
+//執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
+$statement = $pdo->prepare($sql);
+// $statement->bindValue(1, $memberID);
+$statement->execute(); //執行
+
+//抓出全部且依照順序封裝成一個二維陣列
+$data = $statement->fetchAll();
+
+//將二維陣列取出顯示其值
+echo json_encode($data);
+?>
