2741ca8c1576e228dae25a9d8593d3821342a1c9
emily830101 <s603860725@gmail.com>
2023-01-28 23:48:19 +0800

    後台訂單管理2隻基本頁面php，把資料從資料庫帶入html

---
+++ backend/php/detail_order.php
@@ -0,0 +1,48 @@
+<?php
+//取得請求參數
+$orderID = $_GET['orderID'];
+
+
+//DB連線資訊
+include("connection.php");
+
+//建立SQL語法
+$sql = "SELECT `Order`.OrderDate, `Order`.TotalPrice, `Member`.MemberNumber, `Member`.username
+from `Order`
+join `Member`
+on `Order`.MemberID = `Member`.MemberID
+where `Order`.OrderID = ?;";
+
+
+// //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
+$statement = $pdo->prepare($sql);
+$statement->bindValue(1, $orderID);
+$statement->execute(); //執行
+
+// //抓出全部且依照順序封裝成一個二維陣列
+$data = $statement->fetch();
+
+
+//建立SQL語法
+$sql = "SELECT Product.ProductNumber, Product.ProductName, OrderDetail.Quantity, Product.ProductPrice, OrderDetail.SubPrice
+from Product
+inner join OrderDetail
+on Product.ProductID = OrderDetail.ProductID
+where OrderDetail.OrderID = ?;";
+
+
+
+
+//執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
+$statement = $pdo->prepare($sql);
+$statement->bindValue(1, $orderID);
+$statement->execute(); //執行
+
+//抓出全部且依照順序封裝成一個二維陣列
+$orderDetailList = $statement->fetchAll();
+
+$data['OrderDetailList'] = $orderDetailList;
+
+//將結果回傳
+echo json_encode($data);
+?>
---
+++ backend/php/query_order.php
@@ -0,0 +1,56 @@
+<?php
+
+//DB連線資訊
+include("connection.php");
+
+//建立SQL語法
+$sql = [];
+if (isset($_GET['memberNumber'])) { //分辨你是不是按了搜尋
+    //1. 取得請求參數
+    $orderNumber = $_GET['orderNumber'];
+    $memberNumber = $_GET['memberNumber'];
+    $orderStatus = $_GET['orderStatus'];
+    $from = $_GET['from'];
+    $to = $_GET['to'];
+
+    $sql = "SELECT  MemberNumber, OrderDate, PaymentStatus, OrderNumber, OrderID 
+    from `Order` 
+    join Member
+    on `Order`.MemberID = Member.MemberID 
+    WHERE 1 = 1";
+    $criteria = "";
+    if (strlen($memberNumber) != 0){
+        $criteria = $criteria . " AND MemberNumber = '". $memberNumber."'";
+    }
+    if (strlen($orderNumber) != 0){
+        $criteria = $criteria . " AND OrderNumber = '". $orderNumber."'";
+    }
+    if (strlen($orderStatus) != 0){
+        $criteria = $criteria . " AND PaymentStatus = '". $orderStatus."'";
+    } 
+    if (strlen($from) != 0 && strlen($to) != 0){
+        $criteria = $criteria . " AND OrderDate BETWEEN '". $from ."' and '". $to ."'";
+    } 
+
+    $sql = $sql . $criteria . ";";
+
+} 
+else {
+    $sql = "SELECT  MemberNumber, OrderDate, PaymentStatus, OrderNumber, OrderID  
+    from `Order`
+    join Member
+    on `Order`.MemberID = Member.MemberID;";
+}
+
+
+
+//執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
+$statement = $pdo->prepare($sql);
+$statement->execute(); //執行
+
+//抓出全部且依照順序封裝成一個二維陣列
+$data = $statement->fetchAll();
+
+//將二維陣列取出顯示其值
+echo json_encode($data);
+?>
