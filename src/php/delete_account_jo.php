68d3d8166c23139eaa0c49bb6a60a8fbdd68e15c
emily830101 <s603860725@gmail.com>
2023-01-29 13:37:31 +0800

    完成會員中心我的揪團垃圾桶的刪除功能

--- src/php/delete_account_jo.php
+++ src/php/delete_account_jo.php
@@ -1,19 +1,17 @@
 <?php
 include("./Member.php");        
 $memberID = getMemberID();
+$joID = $_GET['joID'];
 
 //DB連線資訊
 include("connection.php");
 
 //建立SQL語法
-// $sql = "SELECT * FROM Order WHERE MemberID = ?";
-$sql = "SELECT Jo.JoTitle, Jo.Location, Jo.JoDetailedAddressed, Jo.JoStartDate, Jo.JoID
-from Jo 
-WHERE MemberID = ?;";
+$sql = "DELETE FROM Jo WHERE JoID = ?";
 
 //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
 $statement = $pdo->prepare($sql);
-$statement->bindValue(1, $memberID);
+$statement->bindValue(1, $joID);
 $statement->execute(); //執行
 
 //抓出全部且依照順序封裝成一個二維陣列
@@ -21,4 +19,7 @@ $data = $statement->fetchAll();
 
 //將二維陣列取出顯示其值
 echo json_encode($data);
+
+//頁面導回HTML
+header("Location: ../account_jo.html");
 ?>
