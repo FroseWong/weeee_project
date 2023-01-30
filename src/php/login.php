93fb15797a7d0d1c1973202926b0eda67c5604c8
ChuYuanJyun <chusmile1021@gmail.com>
2023-01-30 15:55:04 +0800

    change DB info

--- src/php/login.php
+++ src/php/login.php
@@ -2,9 +2,9 @@
    function getPDO(){
 
     $db_host = "127.0.0.1";
-    $db_user = "root";
-    $db_pass = "password";
-    $db_select = "weeee";
+    $db_user = "tibamefe_since2021";
+    $db_pass = "vwRBSb.j&K#E";
+    $db_select = "tibamefe_tgd103g2";
    
     //建立資料庫連線物件
     $dsn = "mysql:host=".$db_host.";dbname=".$db_select;
