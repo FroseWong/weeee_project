f0a5f7e357a1ccba5940e1b1b916752e72ed9868
ChuYuanJyun <chusmile1021@gmail.com>
2023-01-30 16:36:10 +0800

    db

--- src/php/login.php
+++ src/php/login.php
@@ -1,20 +1,21 @@
 <?php
-   function getPDO(){
+//    function getPDO(){
 
-    $db_host = "127.0.0.1";
-    $db_user = "tibamefe_since2021";
-    $db_pass = "vwRBSb.j&K#E";
-    $db_select = "tibamefe_tgd103g2";
+//     $db_host = "127.0.0.1";
+//     $db_user = "tibamefe_since2021";
+//     $db_pass = "vwRBSb.j&K#E";
+//     $db_select = "tibamefe_tgd103g2";
    
-    //建立資料庫連線物件
-    $dsn = "mysql:host=".$db_host.";dbname=".$db_select;
+//     //建立資料庫連線物件
+//     $dsn = "mysql:host=".$db_host.";dbname=".$db_select;
    
-    //建立PDO物件，並放入指定的相關資料
-    $pdo = new PDO($dsn, $db_user, $db_pass);
+//     //建立PDO物件，並放入指定的相關資料
+//     $pdo = new PDO($dsn, $db_user, $db_pass);
 
-    return $pdo;
+//     return $pdo;
     
-}
+// }
+include("./connection.php");
 
     //建立SQL
     $sql = "SELECT * FROM weeee.Member WHERE  Username = ? and Password = ?";
