16539f9e5851916430fca9fe697d69d66fbccc53
Hans <rc40214@gmail.com>
2023-02-02 06:31:53 +0800

    detailcart update

--- src/php/ProductDetailShoppingCart.php
+++ src/php/ProductDetailShoppingCart.php
@@ -1,6 +1,6 @@
 <?php
 require_once('connection.php');
-
+error_reporting(0);
 // -----------取得會員---------------
 include("Member.php");
 getMemberID();
@@ -9,14 +9,12 @@ if (empty($_SESSION["MemberID"])) {
     echo json_encode('NotFound');
     return;
 }
-$MID=$_SESSION["MemberID"];
-// $MID = $_POST['MID'];
+$MID = $_SESSION["MemberID"];
 $PID = $_POST['PID'];
-// $NEWTAL = $_POST['TAL'];
 $NEWQTY = $_POST['QTY'];
-$DATE=$_POST['DATE'];
+$DATE =  (string)$_POST['DATE'];
 $sqlif = "SELECT count(ProductID) From Cart  where MemberID='{$MID}' and ProductID='{$PID}'";
-
+$temp;
 $statementif = $pdo->prepare($sqlif);
 
 $statementif->execute();
@@ -26,40 +24,49 @@ $dataif = $statementif->fetchAll();
 foreach ($dataif as $key => $value) {
     $tempif = $value[0];
 }
-// echo ($tempif);
 // -----------如果使用者沒加過購物車---------------
 if ($tempif == 0) {
     $sqlzero = "INSERT INTO Cart (ProductID,MemberID,Quantity,CartStartDay)VALUES ('{$PID}','{$MID}','{$NEWQTY}','$DATE')";
     $statement = $pdo->prepare($sqlzero);
     $statement->execute();
     echo json_encode('ok');
+    return;
 }
-// -----------如果使用者有加過購物車,先找已加入個數---------------
-if ($tempif == 1) {
-    $sqlsel = "SELECT Quantity FROM Cart where ProductID='{$PID}' AND MemberID='{$MID}'";
-    $statement = $pdo->prepare($sqlsel);
+// -----------如果使用者有加過購物車---------------
+if ($tempif > 0) {
+    // -----------找尋已加入購物車商品之時間---------------
+    $sqldate = "SELECT CartID,count(CartID) FROM Cart  where ProductID='{$PID}' AND MemberID='{$MID}' AND CartStartDay='$DATE' GROUP BY CartID";
+    $statement = $pdo->prepare($sqldate);
     $statement->execute();
-    $datasel = $statement->fetchAll();
-    foreach ($datasel as $key => $value) {
-
-        $OLDQTY = $value[$key];
+    $datadate = $statement->fetchAll();
+    foreach ($datadate as $key => $value) {
+        $temp = $value[0];
     }
-    $ALLQTY = $OLDQTY + $NEWQTY;
-    // -----------找總額---------------
-    // $sqlseltwo = "SELECT SubTotal FROM Cart where ProductID='{$PID}' AND MemberID='{$MID}'";
-    // $statement = $pdo->prepare($sqlseltwo);
-    // $statement->execute();
-    // $dataseltwo = $statement->fetchAll();
-    // foreach ($dataseltwo as $key => $value) {
+    if ($temp > 0) {
+        $CARTID = $value[0];
+        // -----------日期一樣，加入數量---------------
+        $sqlsel = "SELECT Quantity FROM Cart where CartID='$CARTID'";
+        $statement = $pdo->prepare($sqlsel);
+        $statement->execute();
+        $datasel = $statement->fetchAll();
+        foreach ($datasel as $key => $value) {
 
-    //     $OLDTAL = $value[$key];
-    // }
-    // $ALLTAL = $OLDTAL + $NEWTAL;
-
-
-    // --------加總後加入購物車---------
-    $sqlone = "UPDATE Cart SET Quantity=$ALLQTY WHERE ProductID='{$PID}' AND MemberID='{$MID}'";
-    $statement = $pdo->prepare($sqlone);
-    $statement->execute();
-    echo json_encode('ok');
+            $OLDQTY = $value[$key];
+        }
+        $ALLQTY = $OLDQTY + $NEWQTY;
+        // --------加總後加入購物車---------
+        $sqlone = "UPDATE Cart SET Quantity=$ALLQTY WHERE  CartID='$CARTID'";
+        $statement = $pdo->prepare($sqlone);
+        $statement->execute();
+        echo json_encode('ok');
+        return;
+    }
+    // -----------日期不一樣，加入新的一筆---------------
+    else {
+        $sqlzsame = "INSERT INTO Cart (ProductID,MemberID,Quantity,CartStartDay)VALUES ('{$PID}','{$MID}','{$NEWQTY}','$DATE')";
+        $statement = $pdo->prepare($sqlzsame);
+        $statement->execute();
+        echo json_encode('ok');
+        return;
+    }
 }
